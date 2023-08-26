6f4ae61dab26f0f59d34833a7b2fab365dd1fbbc
Abd Alfadeel Hamdy <abdalfadeelh@gmail.com>
2023-08-27 02:58:25 +0300

    Add Role Example

---
+++ src/element-roles/RoleExample.js
@@ -0,0 +1,20 @@
+const RoleExample = () => {
+  return (
+    <div>
+      <a href="/">Link</a>
+      <button>Button</button>
+      <footer>Content info</footer>
+      <h1>Heading</h1>
+      <header>Banner</header>
+      <img alt="description" /> Img
+      <img type="checkbox" /> Checkbox
+      <img type="number" /> Spinbutton
+      <img type="radio" /> Radio
+      <img type="text" /> Textbox
+      <li>Listitem</li>
+      <ul>Listgroup</ul>
+    </div>
+  );
+};
+
+export default RoleExample;
